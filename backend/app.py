b2fe95b188011e568a17db632d8b4a0d67c3cd26
Tomer Shafir <tomershafir@Tomers-MacBook-Pro.local>
2022-09-12 13:49:55 +0300

    add contenarization,nginx & react frontend

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,32 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from db import AttendanceDB
 from env import env_config
 
 app = Flask(__name__)
+CORS(app, origins="*")
+
 attendance_db = AttendanceDB()
 attendance_db.connect()
 attendance_db.load_db_from_participants_file()
 
 
-@app.route("/api/attendees")
+@app.route("/attendees")
 def attendess():
     attendees = attendance_db.get_all_attendees()
     return jsonify(attendees)
 
 
-@app.route("/api/attendance")
+@app.route("/attendance")
 def attendance():
     attendance = attendance_db.get_attendance()
     return jsonify(attendance)
+
+
+@app.route('/test')
+def test():
+    return jsonify({'Test': 'test'})
 
 
 if __name__ == '__main__':
